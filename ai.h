5d4d4b786d2054a45fec6c45b3d786c27fd18c45
Justin Ricks <justin.rixx@gmail.com>
2015-12-14 19:22:23 -0700

    Create a stub AI class that can be overridden to provide AI functionality

---
+++ ai.h
@@ -0,0 +1,39 @@
+/*****************************************************
+ * AI Class
+ * This class is essentially just an interface. To make
+ * a concrete AI, extend this class and implement the
+ * move() method.
+ *****************************************************/
+ #include "ship.h"
+
+ #ifndef _AI_H
+ #define _AI_H
+
+class AI
+{
+public:
+	AI() : isUpPressed(false), isLeftPressed(false), isRightPressed(false),
+		isSpacePressed(false) {};
+
+	/* Update the AI. Don't touch this. Your hook-in point is the move method */
+	void update(Ship & playerShip) { getClosestRocks(); move(playerShip); };
+
+protected:
+	/* OVERRIDE THIS */
+	void move(Ship & playerShip);
+
+	/* Don't touch this */
+	void getClosestRocks();
+
+	// TODO The AI needs a notion of enemy rocks
+	// Rock[5] closestRocks;
+	// TODO the AI needs a pointer to the game
+
+	// a boolean to keep track of what buttons to push at each frame
+	bool isUpPressed;
+	bool isLeftPressed;
+	bool isRightPressed;
+	bool isSpacePressed;
+};
+
+ #endif // _AI_H
