ed172f137377aeaf3971b1327c010f6a226aefa3
Justin Ricks <justin.rixx@gmail.com>
2015-12-23 15:02:39 -0700

    Add a skeleton of the game driver

---
+++ gameDriver.cpp
@@ -0,0 +1,44 @@
+#include "game.h"
+#include "simpleAI.h"
+#include "uiDraw.h"
+#include "uiInteract.h"
+
+using namespace std;
+
+/*************************************
+ * Super simple callback function. Just
+ * update the game, then draw it.
+ **************************************/
+void callBack(const Interface *pUI, void *p)
+{
+	// cast the pointer back to the correct type
+  Game * pGame = (Game *)p;
+
+  // update the game
+  pGame->update(pUI);
+
+  // draw the game
+  pGame->draw();
+
+}
+
+/***********************************************
+ * Main has to initialize the objects to be used
+ * and then passes control to the Interface class.
+ **********************************************/
+int main(int argc, char ** argv)
+{
+  // instantiate the game object
+  Game * pGame = new Game();
+
+  // instantiate an AI
+  SimpleAI ai(pGame);
+
+  // the game needs a reference to the AI
+  pGame->setAI(&ai);
+
+  Interface ui(argc, argv, "Game Test");
+  ui.run(callBack, pGame);
+
+  return 0;
+}
